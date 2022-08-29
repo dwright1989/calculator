0a151e94be934c77bce5e9023a3c8e8f27bc8b51
Debbie Wright <debbiewright@live.co.uk>
2022-08-29 16:26:04 +0100

    Separated button event listeners for better logic

--- script.js
+++ script.js
@@ -1,124 +1,94 @@
 /*
 Global Variables 
 */
-let displayValue = 0;
-let answerValue = 0;
+let upperDisplayValue = 0;
+let bottomDisplayValue = 0;
 let firstNumber = 0;
 let secondNumber = 0;
-let theOperator = 0;
-let firstOperative = true;
-let lastButton = 0;
-let operatorPressed = false;
+let answer = 0;
+let theOperator = "";
+let clearBottom = false;
 
-
-// add event listener for all number buttons
-let btns = document.getElementsByClassName('allButtons');
-console.log("btns: "+ btns.length);
-for (i of btns) {
+// Number Event Listeners
+const numberBtns = document.getElementsByClassName("numberButton");
+for(i of numberBtns){
   i.addEventListener('click', function() {
-      buttonPressed(this);
+    composeNumbers(this.id);
+});
+}
+
+// Operator Event Listeners
+const operatorBtns = document.getElementsByClassName("operatorButton");
+for(i of operatorBtns){
+  i.addEventListener('click', function(){
+    setOperator(this.id);
   });
 }
 
-// add event listener for clear
-let clearButton = document.getElementById("clear");
-clearButton.addEventListener('click', function(){
+// Clear Event Listener
+const clearBtn = document.getElementById("clear");
+clearBtn.addEventListener('click', function(){
   clearScreen();
 });
 
-// add event listener for delete
+// Equal Event Listener
+const equalBtn = document.getElementById("=");
+equalBtn.addEventListener('click', function(){
+  evaluateAnswer();
+});
 
-function buttonPressed(element){
-    let value = element.id;
-    let newTotal = 0;
+// Delete Event Listener
 
-    if(value=="="){
-      console.log("the first number is: " + firstNumber + " the secondnumber is " + secondNumber + " the answer is  " + answerValue);
-      firstNumber = secondNumber;
-      secondNumber = answerValue;
-      console.log("the first number is: " + firstNumber + " the secondnumber is " + secondNumber + " the answer is  " + answerValue);
-      evaluateAnswer();
-    }
-    else{
-      
-      if(value >=0 && value<=9){
-          // If the button pressed is a number then update the answer screen
-          if(isOperator(lastButton)){
-              answerValue = value;
-          }
-          else{
-            answerValue = answerValue.toString() + value.toString(); 
-            answerValue = (answerValue.toString()).replace(/^0+/, '');  
-            secondNumber = answerValue;
-            newTotal = answerValue;
-          }
-      }else if(isOperator(value)){
-        if(isOperator(lastButton)){
-          alert("Please select a number!");
-        }else{
-          theOperator = value;
-          if(firstOperative){
-            displayValue = displayValue + answerValue;
-            displayValue = displayValue + value;
-            answerValue = 0;
-            firstOperative = false;
-          }else{
-            firstNumber = secondNumber;
-            secondNumber = answerValue;
-            
-            newTotal = evaluateAnswer();
-            console.log("the new total: " + newTotal);
-            //answerValue = newTotal;
-            displayValue = newTotal;
-          }
-        }
-        
-      }
-      answerValue = (answerValue.toString()).replace(/^0+/, '');
-      displayValue = (displayValue.toString()).replace(/^0+/, '');
-      console.log("#############################");
-      console.log("End of Button Pressed.  Here are the values.");
-      console.log("First Number: " + firstNumber);
-      console.log("Second Number: " + secondNumber);
-      console.log("Answer Value: " + answerValue);
-      console.log("Display Value: " + displayValue);
-      console.log("#############################");
-    // newTotal = (newTotal.toString()).replace(/^0+/, '');
-      //newTotal = parseInt(newTotal);
-      updateAnswer(answerValue);
-      updateDisplay(displayValue);
-      
-    }
-    lastButton = value;
-}
+// Period Event Listener
 
-function isOperator(val){
-  console.log("in the is operator function.  Valu is: " + val);
-  if(val=="/" || val =="+" ||val=="-" || val=="*"){
-    console.log("about to return true");
-    return true;
+function composeNumbers(value){
+  if(bottomDisplayValue==0 || clearBottom){
+    bottomDisplayValue = "";
+    updateAnswer(bottomDisplayValue);
+    clearBottom = false;
   }
+  bottomDisplayValue = bottomDisplayValue + value;
+  updateAnswer(bottomDisplayValue);
 }
+
+function setOperator(value){
+
+ if(theOperator!=null && theOperator!=""){
+    evaluateAnswer();
+  }
+  firstNumber = bottomDisplayValue;
+  theOperator = value;
+  upperDisplayValue = `${firstNumber} ${theOperator}`;
+  
+  updateDisplay(upperDisplayValue);
+  clearBottom = true;
+  
+  
+}
+
+function evaluateAnswer(){
+  secondNumber = bottomDisplayValue;
+  console.log("first number: " + firstNumber + " second number " + secondNumber + " the operator " + theOperator);
+  let totalSum = operate(firstNumber, secondNumber, theOperator);
+  bottomDisplayValue = totalSum;
+  updateAnswer(bottomDisplayValue);
+  updateDisplay(bottomDisplayValue);
+  theOperator = null;
+}
+
+
 
 function clearScreen(){
-  displayValue = "";
-  answerValue = 0;
+  upperDisplayValue = "";
+  bottomDisplayValue = 0;
   firstNumber = 0;
   secondNumber = 0;
   theOperator = 0;
   firstOperative = true;
-  updateAnswer(answerValue);
-  updateDisplay(displayValue);
+  updateAnswer(bottomDisplayValue);
+  updateDisplay(upperDisplayValue);
 }
     
-
-function evaluateAnswer(){
-  let sumTotal = operate(firstNumber, secondNumber, theOperator);
-  answerValue = sumTotal;
-  secondNumber = sumTotal;
-  updateAnswer(sumTotal);
-  return sumTotal;
-}
 
 function updateDisplay(value){
   document.getElementById("calculator").innerHTML = value;
@@ -132,7 +102,9 @@ function updateAnswer(value){
 
 
 function add(numOne, numTwo) {
+  console.log("about to add the numbers: " + parseInt(numOne) + parseInt(numTwo));
     let sum = parseInt(numOne) + parseInt(numTwo);
+    console.log("the sum is: " + sum);
     return sum;
 };
 
